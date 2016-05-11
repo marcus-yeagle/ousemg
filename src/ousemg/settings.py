c9a90ff72e426e4a22b5668dc41af14245119727
Marcus Yeagle <marcus@Marcuss-MacBook-Pro.local>
2016-05-11 11:23:15 -0400

    updated database url

--- src/ousemg/settings.py
+++ src/ousemg/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,16 +76,18 @@ WSGI_APPLICATION = 'ousemg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-            'NAME': 'ousemg',                                   
-            'USER': 'postgres',
-            'PASSWORD': 'j8b7088Q!',
-            'HOST': '',                                         
-            'PORT': '',                      
-        }
-    }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+#             'NAME': 'ousemg',                                   
+#             'USER': 'postgres',
+#             'PASSWORD': 'j8b7088Q!',
+#             'HOST': '',                                         
+#             'PORT': '',                      
+#         }
+#     }
+
+DATABASES['default'] =   dj_database_url.config(default='postgres://mcifpdejtkixfp:YSiD2yPqa7Q-yyVO6AkNkTcVia@ec2-54-243-249-149.compute-1.amazonaws.com:5432/depf0dvnup3223')
 
 
 
